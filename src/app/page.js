11b34d2ee27eae15d32efcd6b5f59e226eb7f2d6
drkoop <dr.koop.development@gmail.com>
2023-06-12 23:47:10 -0600

    martes deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import { PrismaClient } from "@prisma/client"
 
 
 export default function Home() {
   return (
     <h1>test</h1>
   )
+}
+
+
+export const getServerSideProps = async() => {
+
+  const prisma = new PrismaClient();
+
+  const categorias = await prisma.categoria.findMany();
+
+  console.log(categorias)
+
 }
